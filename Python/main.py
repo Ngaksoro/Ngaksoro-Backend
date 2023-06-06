2f007ea0d67d4d7aa4ea5c8252b40246d920d1a2
Irfan Rosyadi <screetgz@gmail.com>
2023-06-06 18:36:39 +0700

    update

--- Python/main.py
+++ Python/main.py
@@ -2,24 +2,31 @@ import os
 from flask import Flask, request, jsonify
 from google.cloud import storage
 from google.oauth2 import service_account
+from datetime import datetime
 
 
 app = Flask(__name__)
-key_path = 'bucket-read.json'
+key_path = 'ngaksoro-key.json'
 credentials = service_account.Credentials.from_service_account_file(
     key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 storage_client = storage.Client(credentials=credentials)
 bucket_name = 'ngaksoro'
+folder_png = 'aksarajawa'
+folder_gif = 'assetgif'
 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Menerima file gambar dari permintaan POST
     image_file = request.files['file']
+    # Mendapatkan timestamp saat ini
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     # Menyimpan file gambar di bucket Google Cloud Storage
+    file_extension = image_file.filename.rsplit('.', 1)[1]  # Mendapatkan ekstensi file
+    file_name = f"{timestamp}.{file_extension}"
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(image_file.filename)
+    blob = bucket.blob(f"ml/{file_name}")
     blob.upload_from_file(image_file)
 
     return 'File gambar berhasil diunggah ke bucket'
@@ -30,24 +37,32 @@ def get_image_url(filename):
     # Membuat URL file gambar dari bucket Google Cloud Storage
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(filename)
-    # url = blob.generate_signed_url(expiration=300, method='GET')
-    print(storage_client.bucket)
+    return jsonify({'images': 'https://storage.googleapis.com/ngaksoro/aksarajawa/'+ blob.name.lower() +'.png',
+                    'gif': 'https://storage.googleapis.com/ngaksoro/assetgif/'+ blob.name.lower() +'.gif'
+                    })
 
-    return jsonify({'url': 'https://storage.googleapis.com/ngaksoro/'+ blob.name })
-
-@app.route('/files', methods=['GET'])
+@app.route('/assets', methods=['GET'])
 def list_files():
     # Mendapatkan daftar nama file dari bucket Google Cloud Storage
     bucket = storage_client.bucket(bucket_name)
-    blobs = bucket.list_blobs()
-    file_list = []
-    for blob in blobs:
-        file_link = 'https://storage.googleapis.com/{}/{}'.format(bucket_name, blob.name)
-        file_name = blob.name.split("/")[1].split(".")[0]
-        file_data = {'image': file_link, 'text': file_name}
-        file_list.append(file_data)
-    response = {'images': file_list}
-
+    image_png = bucket.list_blobs(prefix=folder_png)
+    image_gif = bucket.list_blobs(prefix=folder_gif)
+    png_list = []
+    gif_list = []
+    for x in image_png:
+        png_link = 'https://storage.googleapis.com/{}/{}'.format(bucket_name, x.name)
+        png_name = x.name.split("/")[1].split(".")[0]
+        png_data = {'image': png_link, 'text': png_name}
+        png_list.append(png_data)
+    for y in image_gif:
+        gif_link = 'https://storage.googleapis.com/{}/{}'.format(bucket_name, y.name)
+        gif_name = y.name.split("/")[1].split(".")[0]
+        gif_data = {'image': gif_link, 'text': gif_name}
+        gif_list.append(gif_data)
+    response = {
+        'images': png_list,
+        'gif': gif_list,
+    }
     return jsonify(response)
 
 if __name__ == '__main__':
--- Python/send.py
+++ Python/send.py
@@ -2,7 +2,7 @@ import requests
 
 # Mengirim file gambar ke API
 def send_image(file_path):
-    url = "https://ngaksoro-s6jxliwbja-uc.a.run.app/upload"  # Ganti dengan URL API yang sesuai
+    url = "http://192.168.1.10:8080/upload"  # Ganti dengan URL API yang sesuai
     files = {'file': open(file_path, 'rb')}
     response = requests.post(url, files=files)
     print(response.text)
