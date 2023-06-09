6181956e61095f74aad06f85d74b15eff1dfb60a
Irfan Rosyadi <screetgz@gmail.com>
2023-06-09 14:26:48 +0700

    Update

--- Python/main.py
+++ Python/main.py
@@ -12,10 +12,14 @@ credentials = service_account.Credentials.from_service_account_file(
 )
 storage_client = storage.Client(credentials=credentials)
 bucket_name = 'ngaksoro'
-folder_png = 'aksarajawa'
-folder_gif = 'assetgif'
+folder_asset = 'aksarajawa'
+folder_ml = 'ml'
 
+@app.route('/', methods=['GET'])
+def index():
+    return 'Halo gaes'
 
+#Upload file dari client dan disimpan ke dalam bucket
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Menerima file gambar dari permintaan POST
@@ -31,37 +35,65 @@ def upload_file():
 
     return 'File gambar berhasil diunggah ke bucket'
 
-
-@app.route('/image/<filename>', methods=['GET'])
-def get_image_url(filename):
-    # Membuat URL file gambar dari bucket Google Cloud Storage
+#Menampilkan semua file yang telah di upload
+@app.route('/ml-storage', methods=['GET'])
+def ml_files():
+    # Mendapatkan daftar nama file dari bucket Google Cloud Storage
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(filename)
-    return jsonify({'images': 'https://storage.googleapis.com/ngaksoro/aksarajawa/'+ blob.name.lower() +'.png',
-                    'gif': 'https://storage.googleapis.com/ngaksoro/assetgif/'+ blob.name.lower() +'.gif'
-                    })
+    blob = bucket.list_blobs(prefix=folder_ml)
+    file_list = []
+    for x in blob:
+        file_link = 'https://storage.googleapis.com/{}/{}'.format(bucket_name, x.name)
+        file_data = {'assets': file_link}
+        file_list.append(file_data)
+    response = {
+        'images': file_list,
+    }
+    return jsonify(response)
 
+#Menghapus semua file yang telah di upload
+@app.route('/ml-destroy', methods=['GET'])
+def delete_files():
+    bucket = storage_client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=folder_ml)
+
+    for blob in blobs:
+        blob.delete()
+
+    return jsonify({'status': 'semua file telah dihapus'})
+
+#Menampilkan file png dan gif sesuai dengan nama huruf aksara
+# @app.route('/image/<filename>', methods=['GET'])
+# def get_image_url(filename):
+#     # Membuat URL file gambar dari bucket Google Cloud Storage
+#     bucket = storage_client.bucket(bucket_name)
+#     blob = bucket.blob(filename)
+#     return jsonify({'images': 'https://storage.googleapis.com/ngaksoro/aksarajawa/'+ blob.name.lower() +'.png',
+#                     'gif': 'https://storage.googleapis.com/ngaksoro/assetgif/'+ blob.name.lower() +'.gif'
+#                     })
+
+#Menampilkan semua file asset yang dibutuhkan android
 @app.route('/assets', methods=['GET'])
 def list_files():
     # Mendapatkan daftar nama file dari bucket Google Cloud Storage
     bucket = storage_client.bucket(bucket_name)
-    image_png = bucket.list_blobs(prefix=folder_png)
-    image_gif = bucket.list_blobs(prefix=folder_gif)
-    png_list = []
-    gif_list = []
-    for x in image_png:
-        png_link = 'https://storage.googleapis.com/{}/{}'.format(bucket_name, x.name)
-        png_name = x.name.split("/")[1].split(".")[0]
-        png_data = {'image': png_link, 'text': png_name}
-        png_list.append(png_data)
-    for y in image_gif:
-        gif_link = 'https://storage.googleapis.com/{}/{}'.format(bucket_name, y.name)
-        gif_name = y.name.split("/")[1].split(".")[0]
-        gif_data = {'image': gif_link, 'text': gif_name}
-        gif_list.append(gif_data)
+    image_folder = bucket.list_blobs(prefix=folder_asset)
+    img_list = []
+    aksara_order = ['ha', 'na', 'ca', 'ra','ka', 'da', 'ta', 'sa', 'wa', 'la', 'pa', 'dha', 'ja', 'ya', 'nya', 'ma', 'ga', 'ba', 'tha', 'nga']
+    alphabet_order = []
+    for x in image_folder:
+        img_name = x.name.split("/")[1].split(".")[0]
+        alphabet_order.append(img_name)
+    for y in alphabet_order:
+            index = alphabet_order.index(y)
+            aksara_jawa_char = aksara_order[index]
+            img_data = {
+                'image': 'https://storage.googleapis.com/ngaksoro/aksarajawa/'+ aksara_jawa_char +'.png', 
+                'gif': 'https://storage.googleapis.com/ngaksoro/assetgif/'+ aksara_jawa_char +'.gif',
+                'text': aksara_jawa_char}
+            img_list.append(img_data)
     response = {
-        'images': png_list,
-        'gif': gif_list,
+        'images': img_list,
     }
     return jsonify(response)
 
